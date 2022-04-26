123f34d2c53f4a2778265519b9e871d4fc41833b
codi kingsbury <codikingsbury117@gmail.com>
2022-04-26 09:40:58 -0700

    Endpoints started for get by Id and Get all Video Games

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,32 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{   //api/Games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetVideoGame()
+        {
+            var videoGame = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+
+            return Ok(videoGame);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetGamesbyId(int Id)
+        {
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == Id); 
+            return Ok();
+        }
+    }
+}
